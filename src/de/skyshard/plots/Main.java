cf16f32434c59a6d72d8369fb6b4414e55300bb1
jonas-git <jonas.vanen@gmail.com>
2019-01-02 14:15:42 +0100

    Created Main source file

---
+++ src/de/skyshard/plots/Main.java
@@ -0,0 +1,22 @@
+package de.skyshard.plots;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+    /**
+     * Fired when the server enables the plugin.
+     */
+    @Override
+    public void onEnable()
+    {
+    }
+
+    /**
+     * Fired when the server stops and disables all plugins.
+     */
+    @Override
+    public void onDisable()
+    {
+    }
+}
