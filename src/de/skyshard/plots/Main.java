e5a30faa4effb9e329401d5cdad3304e2ba19062
jonas-git <jonas.vanen@gmail.com>
2019-01-02 15:02:45 +0100

    Added WorldGuard dependency

--- src/de/skyshard/plots/Main.java
+++ src/de/skyshard/plots/Main.java
@@ -1,5 +1,8 @@
 package de.skyshard.plots;
 
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin
@@ -10,6 +13,16 @@ public class Main extends JavaPlugin
     @Override
     public void onEnable()
     {
+        PluginManager pluginManager = getServer().getPluginManager();
+
+        final String pluginName = "WorldGuard";
+        Plugin worldGuard = pluginManager.getPlugin(pluginName);
+
+        if (worldGuard == null || !(worldGuard instanceof WorldGuardPlugin))
+        {
+            System.out.println("[Plots] missing plugin dependency: " + pluginName);
+            pluginManager.disablePlugin(this);
+        }
     }
 
     /**
